0f560c3d02f4c40573084dc39a625596d4b365b3
Anubhav Saxena <tuthubpro@gmail.com>
2018-07-03 17:22:00 +0530

    no of users allowed introduced

--- constants/consts.js
+++ constants/consts.js
@@ -1,7 +1,7 @@
 exports.subscriptionTypes = [
-    {'type': 'Demo', 'days': 7},
-    {'type': 'Monthly', 'days': 30},
-    {'type': 'Half-Yearly', 'days': 182.5},
-    {'type': 'Yearly', 'days': 365},
-    {'type': 'Tri-Yearly', 'days': 3 * 365}
+    {'type': 'Demo', 'days': 7, 'userAllowed': 1},
+    {'type': 'Monthly', 'days': 30, 'userAllowed': 3},
+    {'type': 'Half-Yearly', 'days': 182.5, 'userAllowed': 5},
+    {'type': 'Yearly', 'days': 365, 'userAllowed': 7},
+    {'type': 'Tri-Yearly', 'days': 3 * 365, 'userAllowed': 'unlimited'}
 ];
